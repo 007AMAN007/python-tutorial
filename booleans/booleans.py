1d4cadc1a411abd9a9f264f59c8affb11a2daefa
007AMAN007 <as47986@gmail.com>
2020-05-30 16:59:17 +0530

    commit 30-05-2020 evening

--- booleans/booleans.py
+++ booleans/booleans.py
@@ -1,1 +1,11 @@
-ss
+#print(10>11) return false
+#print(9>0) return true
+#print(bool("h")) return true
+#print(bool("")) return false
+#print(bool(0)) return false
+#print(bool(1)) return true
+
+#if value inside any data type like string, list etc is null/empty or "" then bool() return false otherwise
+#return true for int if 0 return false for all other retunr true
+
+#operator part skip it's same in all languages
