ed4b148adf9ce5f90cd2788ef4e6732bdb53b81e
007AMAN007 <as47986@gmail.com>
2020-06-04 17:14:13 +0530

    update

--- bank-account-app/bank.py
+++ bank-account-app/bank.py
@@ -6,13 +6,15 @@ class Bank:
     accountBalance = 0
     obj = BankFeature()
     while i < 10:
-        option = raw_input("Enet 1 for deposit, 2 for withdrawl, 0 for exit: ")
+        option = raw_input("Enet 1 for deposit, 2 for withdrawl, 3 for check balance, 0 for exit: ")
         if option == '1':
             obj.add()
         elif option == '2':
             obj.withdrawl()
         elif option == '0':
             exit()
+        elif option == '3':
+            obj.show()    
         else:
           print("Enter valid option")    
         i += 1
--- bank-account-app/bankFeature.py
+++ bank-account-app/bankFeature.py
@@ -5,17 +5,27 @@ class BankFeature:
         print("Enter deposite amount: ")
         depAmt = raw_input()
         obj = bankModule.totalBalance
-        obj = obj+int(depAmt)
-        bankModule.totalBalance = obj
-        print(bankModule.totalBalance)
+        if depAmt.isdigit():
+            obj = obj+int(depAmt)
+            bankModule.totalBalance = obj
+            print(bankModule.totalBalance)
+        else:
+            print("Please enter valid value")
 
     def withdrawl(self):
         print("Enter withdrawl amount: ")
         withAmt = raw_input()
         obj = bankModule.totalBalance
-        if obj<1:
-          print("You don't have sufficient balance")
+        if withAmt.isdigit():
+            if obj<1:
+                print("You don't have sufficient balance")
+            else:
+                obj = obj - int(withAmt)
+                bankModule.totalBalance = obj
+                print(bankModule.totalBalance)
         else:
-            obj = obj - int(withAmt)
-            bankModule.totalBalance = obj
-            print(bankModule.totalBalance)
+            print("Please enter valid value")
+
+    def show(self):
+        print("Your Account balance is: ")
+        print(bankModule.totalBalance)
